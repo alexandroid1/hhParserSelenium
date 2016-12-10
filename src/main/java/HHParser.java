ee1a1088f089c9f19b9b4370b43502fd4d86687c
Oleksandr <avpavlov96@gmail.com>
2016-12-10 16:51:47 +0300

    imports

---
+++ src/main/java/HHParser.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Oleksandr on 10.12.2016.
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HHParser {
+
+
+
+}
